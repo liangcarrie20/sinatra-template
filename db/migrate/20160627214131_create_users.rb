d47604c1202d74805bd7920677c7b94681b3d564
Carrie Liang <20.c.liang@gmail.com>
2016-06-27 22:27:57 -0700

    add first_name to user migration

--- db/migrate/20160627214131_create_users.rb
+++ db/migrate/20160627214131_create_users.rb
@@ -1,6 +1,7 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      t.string :first_name, presence: true
       t.string :username, presence: true
       t.string :email, presence: true, uniqueness: true
       t.string :password_digest, presence:true
