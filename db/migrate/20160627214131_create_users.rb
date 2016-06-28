76fc5caf201aa3f4024c05966a42d963cb9f6725
Carrie Liang <20.c.liang@gmail.com>
2016-06-27 21:49:32 -0700

    add cols for users table with timetamps and validations

--- db/migrate/20160627214131_create_users.rb
+++ db/migrate/20160627214131_create_users.rb
@@ -1,4 +1,11 @@
 class CreateUsers < ActiveRecord::Migration
   def change
+    create_table :users do |t|
+      t.string :username, presence: true
+      t.string :email, presence: true, uniqueness: true
+      t.string :password_digest, presence:true
+
+      t.timestamps(null:false)
+    end
   end
 end
