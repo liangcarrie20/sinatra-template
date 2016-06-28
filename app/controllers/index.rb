4fbcf2106de5f2c62a404d177457271bf39df6d1
Jason Sutter <jasonsutter87@gmail.com>
2016-06-28 10:59:39 -0700

    updated controllers, views, models, validations

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,16 @@
 get '/' do
-  redirect '/users'  #EDIT THIS******** this should redirect to whatever is the main page
+  erb :'index'  #EDIT THIS******** this should redirect to whatever is the main page
+end
+
+
+get '/authorization' do
+	erb :'/authorization/authorization'
+end
+
+get '/logged_in' do
+  if session[:user_id]
+    return "asdkjashkjdh"
+  else
+   false
+ end
 end
--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -8,46 +8,43 @@ get '/users/new' do
   erb :'users/new'
 end
 
-post '/users' do
+post "/users" do
   @user = User.new(params[:user])
+
+  @error = @user.errors.full_messages
+
   if @user.save
-    session[:id] = @user[:id]
-    redirect '/'
+    redirect "/authorization"
   else
-    p "******"
-    p "error"
-    # error handling goes here
-    redirect '/users/new'
+    @error = "Invalid information, double check your email is correct or that you have not already created an account with this email"
+    erb :"/users/new"
   end
 end
 
-# profile page
-get 'users/:id' do
-  @user = User.find(params[:id])
-  erb :'users/show'
+
+get "/users/:id" do
+  @user = current_user
+  erb :"/users/show"
 end
 
-# *****sessions can go here or sessions.rb controller *****
-# AKA login (by email)
-get '/sessions/new' do
-  erb :'sessions/new'
+get '/authorization' do
+  erb :'/authorization/authorization'
 end
 
-post '/sessions' do
-  @user = User.find_by(email params[:user][:email])
-  # if user exists & pw correct (using password digest instead of password hash)
+
+post "/authorization" do
+  @user = User.find_by(email: params[:user][:email])
+  
   if @user && @user.authenticate(params[:user][:password])
-    session[:id] = @user[:id]
-    # redirects to main page ..
-    redirect '/users'
+    session[:user_id] = @user.id
+    redirect "/"
   else
-    # error handling could be handled here..
-    p "Your password and or email was incorrect"
-    redirect '/sessions/new'
+    @error = "Sorry, try a new password or email!"
+    erb :"/authorization/authorization"
   end
 end
 
-# logout
-delete '/sessions/:id' do
-  session[:id] = nil
+get "/signout" do
+  session[:user_id] = nil
+  redirect "/"
 end
