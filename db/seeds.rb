4a6ac150a329bc2907213bbc7768f13a45dfbfe6
Carrie Liang <20.c.liang@gmail.com>
2016-06-27 22:16:08 -0700

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
 10.times do
-  User.create(username: Faker::Name.first_name, email: Faker::Internet.email, password: '123')  
+  User.create!(username: Faker::Name.first_name, email: Faker::Internet.email, password: '123')  
 end
